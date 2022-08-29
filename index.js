197428713a4e03ca3dcc0280d8df4dfc5b7902ea
Ben Scattergood <ben.s1234@hotmail.co.uk>
2022-08-29 19:48:25 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+const btnSubmit = document.getElementById("submit-btn");
+const input = document.querySelector(".form_input");
+const errorIcon = document.getElementById("error-icon")
+const validationMessage = document.querySelector(".validationMessage");
+
+form.addEventListener("submit", Validate);
+btnSubmit.addEventListener("click", Validate);
+
+function Validate(e) {
+    e.preventDefault();
+    console.log(input.value);
+
+    if (isEmail(input.value)) {
+        
+        errorIcon.classList.add("hidden");
+        validationMessage.classList.add("hidden");
+        input.classList.remove("error")
+
+    }
+    else {
+        errorIcon.classList.remove("hidden");
+        validationMessage.classList.remove("hidden");
+        input.classList.add("error");
+    }
+}
+
+function isEmail(input){
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)
+}
